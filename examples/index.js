6b4af115ec20f30342d78d59c018149256cff560
Christoph Jerolimov <jerolimov+git+2015@gmail.com>
2015-11-23 00:19:47 +0100

    Cleanup

--- examples/index.js
+++ examples/index.js
@@ -1,9 +1,8 @@
 
-import React, { Component, View } from 'react-native';
+import React, { Component } from 'react-native';
 import { Provider } from 'react-redux/native';
 
 import store from './store';
-import SelectExample from './SelectExample';
 import NavigationView from './NavigationView';
 
 export default class App extends Component {
